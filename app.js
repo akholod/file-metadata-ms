437626cb9f09c5abe7e369ece2879d98d4be5f1f
akholod <andrey.kokhanevich@gmail.com>
2016-10-28 22:54:15 +0300

    change listen port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const app = express();
 
 // view engine setup
